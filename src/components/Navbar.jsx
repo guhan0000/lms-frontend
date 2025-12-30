9c6ac6a03b20c10c4ed6cd135b9e6b0a914cb9a2
guhan0000 <guhanm0000@gmail.com>
2025-12-30 10:24:52 +0530

    Navbar changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg  bg-primary navbar-dark fixed-top">
-      <div className="container-fluid ">
-        <p className="navbar-brand">Library Management System </p>
+    <nav className="navbar navbar-expand-lg  bg-dark navbar-dark fixed-top">
+      <div className="container">
+        <div className="navbar-brand">LMS</div>
 
         <button
           class="navbar-toggler"
@@ -20,10 +20,12 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <p className="nav-link">Login</p>
+              {/* <p className="nav-link">Login</p> */}
+              <a href="#">Login</a>
             </li>
             <li className="nav-item">
-              <p className="nav-link">Signup</p>
+              {/* <p className="nav-link">Signup</p> */}
+              <a href="#">SignUp</a>
             </li>
           </ul>
         </div>
@@ -33,3 +35,30 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+// const Navbar = () => {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+//       <div className="container">
+//         <p className="text-success">Library System</p>
+//         <div className="collapse navbar-collapse">
+//           <ul className="navbar-nav ms-auto">
+//             <li className="nav-item">Login</li>
+//             <li className="nav-item">Signup</li>
+
+//             <li className="nav-item">Home</li>
+//             <li className="nav-item">Admin Dashboard</li>
+
+//             <li className="nav-item">My Dashboard</li>
+
+//             <li className="nav-item">
+//               <button className="btn btn-link nav-link">Logout</button>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
